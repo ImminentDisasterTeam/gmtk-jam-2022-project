c429c263c1b7be5df3df1282013d082efc5dce19
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2022-07-17 18:50:50 +0300

    Add sound system dummy

---
+++ Assets/_Game/Scripts/SoundHolder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts {
+    public class SoundHolder : MonoBehaviour {
+        [SerializeField] private AudioClip[] _clips;
+
+        public static SoundHolder Instance { get; private set; }
+
+        public SoundHolder() {
+            Instance = this;
+        }
+
+        public void PlaySound(string name, float volume = 1f) {
+            // TODO
+        }
+
+        public void PlayMusic(string name, float volume = 1f) {
+            // TODO
+        }
+    }
+}
